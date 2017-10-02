d0ae91ef858249d3c6d3c4918ea1636288536c85
Chuck Muckamuck <chuck.muckamuck@gmail.com>
2017-10-02 21:31:48 +0000

    Bump version: 0.1.4 → 0.1.5

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name="Stackility",
-    version='0.1.4',
+    version='0.1.5',
     packages=['stackility'],
     description='Python CloudFormation utility',
     author='Chuck Muckamuck',
