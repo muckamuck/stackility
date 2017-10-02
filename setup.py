0b46a0fb30181513c524ba4363ceb06b15410c44
Chuck Muckamuck <chuck.muckamuck@gmail.com>
2017-10-02 21:30:07 +0000

    version is back

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 setup(
     name="Stackility",
+    version='0.1.4',
     packages=['stackility'],
     description='Python CloudFormation utility',
     author='Chuck Muckamuck',
