ab3d2054fc312f43c1ba369fbdf92e1198d76539
Chuck Muckamuck <chuck.muckamuck@gmail.com>
2018-08-23 14:00:08 -0500

    Code improvments/changes

--- stackility/command.py
+++ stackility/command.py
@@ -3,22 +3,27 @@ The command line interface to stackility.
 
 Major help from: https://www.youtube.com/watch?v=kNke39OZ2k0
 """
-from stackility import CloudStackUtility
-from stackility import StackTool
 from configparser import RawConfigParser
-import click
 import time
 import json
-import boto3
 import logging
 import sys
 import os
 import traceback
-
+import boto3
+import click
+from stackility import CloudStackUtility
+from stackility import StackTool
 
 @click.group()
 @click.version_option(version='0.5.5')
 def cli():
+    """
+    Nothing
+
+    Returns:
+       Nothing
+    """
     pass
 
 
@@ -27,10 +32,30 @@ def cli():
 @click.option('--stack', '-s', help='stack name')
 @click.option('--ini', '-i', help='INI file with needed information', required=True)
 @click.option('--dryrun', '-d', help='dry run', is_flag=True)
-@click.option('--yaml', '-y', help='YAML template (deprecated - YAMLness is now detected at run-time', is_flag=True)
+@click.option(
+    '--yaml', '-y',
+    help='YAML template (deprecated - YAMLness is now detected at run-time',
+    is_flag=True
+)
 @click.option('--no-poll', help='Start the stack work but do not poll', is_flag=True)
 @click.option('--work-directory', '-w', help='Start in the given working directory')
 def upsert(version, stack, ini, dryrun, yaml, no_poll, work_directory):
+    """
+    The main reason we have arrived here. This is the entry-point for the
+    utility to update / create an CloudFormation stack
+
+    Args:
+        version - [option] print th version of the utility
+        stack - [optional] specify the CloudFomation stack name
+        ini - [required] specify the path to the INI file for the upsert
+        dryrun - [optional] if found do not pass the template to CloudFormation
+        yaml - [deprecated]
+        no_poll - [optional] send stack to CloudFormation and exit
+        work_directory - [optional] specify where to start
+
+    Returns:
+       nothing
+    """
     ini_data = read_config_info(ini)
     if 'environment' not in ini_data:
         print('[environment] section is required in the INI file')
@@ -44,20 +69,9 @@ def upsert(version, stack, ini, dryrun, yaml, no_poll, work_directory):
     if 'region' not in ini_data['environment']:
         ini_data['environment']['region'] = find_myself()
 
-    if yaml:
-        ini_data['yaml'] = True
-    else:
-        ini_data['yaml'] = False
-
-    if no_poll:
-        ini_data['no_poll'] = True
-    else:
-        ini_data['no_poll'] = False
-
-    if dryrun:
-        ini_data['dryrun'] = True
-    else:
-        ini_data['dryrun'] = False
+    ini_data['yaml'] = bool(yaml)
+    ini_data['no_poll'] = bool(no_poll)
+    ini_data['dryrun'] = bool(dryrun)
 
     if stack:
         ini_data['environment']['stack_name'] = stack
@@ -78,6 +92,17 @@ def upsert(version, stack, ini, dryrun, yaml, no_poll, work_directory):
 @click.option('-r', '--region')
 @click.option('-f', '--profile')
 def delete(stack, region, profile):
+    """
+    Delete the given CloudFormation stack.
+
+    Args:
+        stack - [required] indicatate the stack to smash
+        region - [optional] indicate where the stack is located
+        profile - [optionl[ AWS crendential profile
+
+    Returns:
+       sys.exit() - 0 is good and 1 is bad
+    """
     ini_data = {}
     environment = {}
 
@@ -102,6 +127,16 @@ def delete(stack, region, profile):
 @click.option('-r', '--region')
 @click.option('-f', '--profile')
 def list(region, profile):
+    """
+    List all the CloudFormation stacks
+
+    Args:
+        region - [optional] indicate where the stacks are located
+        profile - [optionl[ AWS crendential profile
+
+    Returns:
+       prints a list of all CloudFormation stacks
+    """
     ini_data = {}
     environment = {}
 
@@ -121,6 +156,16 @@ def list(region, profile):
 
 
 def start_upsert(ini_data):
+    """
+    Helper function to facilitate upsert.
+
+    Args:
+        ini_date - the dictionary of info to run upsert
+
+   Exit:
+       0 - good
+       1 - bad
+    """
     stack_driver = CloudStackUtility(ini_data)
     poll_stack = not ini_data.get('no_poll', False)
     if stack_driver.upsert():
@@ -132,9 +177,9 @@ def start_upsert(ini_data):
                 try:
                     profile = ini_data.get('environment', {}).get('profile')
                     if profile:
-                        b3Sess = boto3.session.Session(profile_name=profile)
+                        boto3_session = boto3.session.Session(profile_name=profile)
                     else:
-                        b3Sess = boto3.session.Session()
+                        boto3_session = boto3.session.Session()
 
                     region = ini_data['environment']['region']
                     stack_name = ini_data['environment']['stack_name']
@@ -142,7 +187,7 @@ def start_upsert(ini_data):
                     cf_client = stack_driver.get_cloud_formation_client()
 
                     if not cf_client:
-                        cf_client = b3Sess.client('cloudformation', region_name=region)
+                        cf_client = boto3_session.client('cloudformation', region_name=region)
 
                     stack_tool = stack_tool = StackTool(
                         stack_name,
@@ -163,21 +208,60 @@ def start_upsert(ini_data):
 
 
 def start_list(command_line):
+    """
+    Facilitate the listing of a CloudFormation stacks
+
+    Args:
+        command_line - a dictionary to of info to inform the operation
+
+    Returns:
+       True if happy else False
+    """
     stack_driver = CloudStackUtility(command_line)
     return stack_driver.list()
 
 
 def start_smash(command_line):
+    """
+    Facilitate the smashing of a CloudFormation stack
+
+    Args:
+        command_line - a dictionary to of info to inform the operation
+
+    Returns:
+       True if happy else False
+    """
     stack_driver = CloudStackUtility(command_line)
     return stack_driver.smash()
 
 
 def find_myself():
+    """
+    Find myself
+
+    Args:
+        None
+
+    Returns:
+       An Amazon region
+    """
     s = boto3.session.Session()
     return s.region_name
 
 
 def read_config_info(ini_file):
+    """
+    Read the INI file
+
+    Args:
+        ini_file - path to the file
+
+    Returns:
+        A dictionary of stuff from the INI file
+
+    Exits:
+        1 - if problems are encountered
+    """
     try:
         config = RawConfigParser()
         config.optionxform = lambda option: option
@@ -193,7 +277,3 @@ def read_config_info(ini_file):
         logging.error('Exception caught in read_config_info(): {}'.format(wtf))
         traceback.print_exc(file=sys.stdout)
         return sys.exit(1)
-
-
-def validate_config_info():
-    return True
