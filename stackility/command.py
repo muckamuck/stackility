471be986e2c9b0eab0a2dbd2962d36daea100208
Chuck Muckamuck <chuck.muckamuck@gmail.com>
2018-11-06 14:50:13 -0600

    Bump version: 0.6.1 → 0.6.2

--- stackility/command.py
+++ stackility/command.py
@@ -16,7 +16,7 @@ from stackility import CloudStackUtility
 from stackility import StackTool
 
 @click.group()
-@click.version_option(version='0.6.1')
+@click.version_option(version='0.6.2')
 def cli():
     """
     Nothing
